abd100ce677fad06921bb8f06e34881e030b3617
Anis Iqbal <anisiqbal484@gmail.com>
2021-08-30 01:22:02 +0500

    working in state, grid,event

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React , {useState} from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button'
+import { Grid, Button, TextField} from '@material-ui/core'
 
 
 function App() {
-  const [color, setcolor]=useState("primary")
-  function customMe(){
+  const [color, setcolor] = useState("primary");
+  const [name, setName] = useState("");
 
-    setcolor("secondary")
-    // alert("function called")
+  function customMe() {
+    // setcolor("secondary");
+     alert("Muhammad Anis");
+    
   }
+
+  function onNameChange(e) {
+    setName(e.target.value);
+    // console.log(e.target.value);
+  }
+
   return (
     <div className="App">
       <h1>React-MUI</h1>
-      <Button 
-      color={color}
-      variant="outlined" 
-      onClick={()=>{customMe()}} 
-      >Click me </Button>
-       
-       
-      
-     
 
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <TextField label="Your Name" onChange={ (e) => {onNameChange(e)} } />
+
+          <label>My Name is {name}</label>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Button 
+          color={color}
+          variant="outlined" 
+          onClick={()=> { customMe() }}>
+            Click me
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
